8646f0f43f22478e48b2210423a02c2eee6e5ef2
Hasan Paşalı <hasan-pasali@hotmail.com>
2023-08-08 17:56:28 +0300

    Update Food.cs

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -6,6 +6,7 @@ public class Food : MonoBehaviour
 {
     // The spawn area is restricted with a empty game object which has a box collider component.
     public BoxCollider2D gridArea;
+    private bool spawnFlag;
 
     private void Start()
     {
@@ -13,6 +14,7 @@ public class Food : MonoBehaviour
     }
     public void RandomizePosition()
     {
+        spawnFlag = false;
         // The food's position is changing with this algorithm. If the spawnPosition == any segment's position this method will work again on the 24th line.
         Vector3 spawnPosition = new Vector3();
         Bounds bounds = gridArea.bounds;
@@ -20,24 +22,29 @@ public class Food : MonoBehaviour
         float y = Random.Range(bounds.min.y, bounds.max.y);
         spawnPosition = new Vector3(Mathf.Round(x), Mathf.Round(y), 0);
 
-        for (int i = GameObject.Find("Snake").GetComponent<Snake>().segments.Count - 1; i > 0; i--) 
+        for (int i = GameObject.Find("Snake").GetComponent<Snake>().segments.Count - 1; i > 0; i--)
         {
-            if(spawnPosition == GameObject.Find("Snake").GetComponent<Snake>().segments[i].position)
+            if (spawnPosition == GameObject.Find("Snake").GetComponent<Snake>().segments[i].position)
             {
-                RandomizePosition();
+                spawnFlag = true;
                 break;
             }
-            else
-            {
-                transform.position = spawnPosition;
-            }
         }
+        if (!spawnFlag)
+        {
+            transform.position = spawnPosition;
+        }
+        else
+        {
+            RandomizePosition();
+        }
+
 
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")) 
+        if (collision.CompareTag("Player"))
         {
             RandomizePosition();
         }
